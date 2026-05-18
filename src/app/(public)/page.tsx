8bcef2c875419d4aca63fb3948e2f21f32dfa92e
Dsmurts <danielsmurts@gmail.com>
2026-05-18 19:58:36 +0300

    ..

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import HomeClient from "./HomeClient";
 
 export const metadata: Metadata = {
@@ -14,5 +15,13 @@ export const metadata: Metadata = {
 };
 
 export default function HomePage() {
-  return <HomeClient />;
+  return (
+    <>
+      <HomeClient />
+      <Script 
+        src="https://cloudpbx.beeline.ru/app/cabinet/app/pub/callmenow/mpbx-cmn-frame.js?externalId=139decae-5201-41bd-a3ee-6897d7175e19-415563598&theme=1&color=1&opened=1"
+        strategy="lazyOnload"
+      />
+    </>
+  );
 }
